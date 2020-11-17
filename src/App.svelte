dbc582e5905566a23b0bcd7a91f0b9bb102702c8
AJ Villalobos <hi@ajvillalobos.com>
2020-11-17 22:29:37 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Allura&display=swap");
+
+	main {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	img {
+		height: 500px;
+		width: auto;
+		border: 1px solid #a0aec0;
+	}
+
+	p {
+		font-family: Allura;
+		font-size: 2rem;
+		color: #2d3748;
+		margin-top: 5px;
+	}
+</style>
+
+<main>
+	<img src="images/christaalcano.jpeg" alt="lady in pink" />
+	<p>Christa Alcano</p>
+</main>
